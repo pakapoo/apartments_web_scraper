cb6a5e6209371bfe4c69cf089cef659a778ba4be
Mark <mark841030@gmail.com>
2024-05-26 23:24:53 +0800

    add functions for extracting information & add multiprocessing to speed up html extraction

--- src/crawler/extraction_functions.py
+++ src/crawler/extraction_functions.py
@@ -1,32 +1,33 @@
 import re
+from datetime import datetime
 
 def name(soup, selector):
     name_soup = soup.select_one(selector)
-    return name_soup.get_text(strip=True) if name_soup else ""
+    return name_soup.get_text(strip=True) if name_soup else None
 
 def tel(soup, selector):
     tel_soup = soup.select_one(selector)
-    return tel_soup.get_text(strip=True) if tel_soup else ""
+    return tel_soup.get_text(strip=True) if tel_soup else None
 
 def address(soup, selector):
     address_soup = soup.select_one(selector)
-    return address_soup.get_text(strip=True) if address_soup else ""
+    return address_soup.get_text(strip=True) if address_soup else None
 
 def city(soup, selector):
     city_soup = soup.select_one(selector)
-    return city_soup.get_text(strip=True) if city_soup else ""
+    return city_soup.get_text(strip=True) if city_soup else None
 
 def state(soup, selector):
     state_soup = soup.select_one(selector)
-    return state_soup.get_text(strip=True) if state_soup else ""
+    return state_soup.get_text(strip=True) if state_soup else None
 
 def zip(soup, selector):
     zip_soup = soup.select_one(selector)
-    return zip_soup.get_text(strip=True) if zip_soup else ""
+    return zip_soup.get_text(strip=True) if zip_soup else None
 
 def neighborhood(soup, selector):
     neighborhood_soup = soup.select_one(selector)
-    return neighborhood_soup.get_text(strip=True) if neighborhood_soup else ""
+    return neighborhood_soup.get_text(strip=True) if neighborhood_soup else None
 
 def built_units_stories(soup, selector):
     unitsstories_soup = soup.select_one(selector)
@@ -34,13 +35,13 @@ def built_units_stories(soup, selector):
         content = unitsstories_soup.get_text()
         built_regex = re.compile(r'(Built in)(\s+)(\d+)')
         built_match = built_regex.search(content)
-        built = built_match.group(3) if built_match else 0
+        built = int(built_match.group(3)) if built_match else 0
         units_regex = re.compile(r'(\d+)(\s+)(units)')
         units_match = units_regex.search(content)
-        units = units_match.group(1) if units_match else 0
+        units = int(units_match.group(1)) if units_match else 0
         stories_regex = re.compile(r'(\d+)(\s+)(stories)')
         stories_match = stories_regex.search(content)
-        stories = stories_match.group(1) if stories_match else 0
+        stories = int(stories_match.group(1)) if stories_match else 0
     else:
         built, units, stories = 0, 0, 0
     return built, units, stories
@@ -53,9 +54,9 @@ def management(soup, selector):
             management_soup = re.split(r'\/|\.', management_soup['src'])[-2]
             management = ' '.join(management_soup.replace('-', ' ').split(' ')[:-2])
         except:
-            management = ""
+            management = None
     else:
-        management = ""
+        management = None
     return management
 
 def beds(soup, selector):
@@ -64,7 +65,7 @@ def beds(soup, selector):
         beds = beds_soup.get_text(strip=True)
         beds_regex = re.compile(r'(\d+)(\s+)(bed)')
         beds_match = beds_regex.search(beds)
-        beds = beds_match.group(1) if beds_match else 0
+        beds = int(beds_match.group(1)) if beds_match else 0
     else:
         beds = 0
     return beds
@@ -75,31 +76,39 @@ def baths(soup, selector):
         baths = baths_soup.get_text(strip=True)
         baths_regex = re.compile(r'(\d+)(\s+)(bath)')
         baths_match = baths_regex.search(baths)
-        baths = baths_match.group(1) if baths_match else 0
+        baths = int(baths_match.group(1)) if baths_match else 0
     else:
         baths = 0
     return baths
 
 def unit_no(soup, selector):
     unit_no_soup = soup.select_one(selector)
-    return unit_no_soup.get_text(strip=True) if unit_no_soup else ""
+    return unit_no_soup.get_text(strip=True) if unit_no_soup else None
 
 def unit_price(soup, selector):
     unit_price_soup = soup.select_one(selector)
-    return int(unit_price_soup.get_text(strip=True).replace('$', '').replace(',', '')) if unit_price_soup else ""
+    if unit_price_soup:
+        try:
+            unit_price_tmp = int(unit_price_soup.get_text(strip=True).replace('$', '').replace(',', ''))
+        except:
+            unit_price_tmp = None
+    return unit_price_tmp if unit_price_soup else None
 
 def unit_sqft(soup, selector):
     unit_sqft_soup = soup.select_one(selector)
-    return int(unit_sqft_soup.get_text(strip=True).replace(',', '')) if unit_sqft_soup else ""
+    return int(unit_sqft_soup.get_text(strip=True).replace(',', '')) if unit_sqft_soup else None
 
 def unit_avail(soup, selector):
     unit_avail_soup = soup.select_one(selector)
+    year = datetime.today().year
     if unit_avail_soup:
-        unit_avail_soup = unit_avail_soup.get_text(strip=True)
+        unit_avail_soup = unit_avail_soup.get_text('|', strip=True).split('|')[-1].strip()
         if unit_avail_soup.split(' ')[-1].isdigit():
-            unit_avail = unit_avail_soup.split(' ')[-2] + ' ' + unit_avail_soup.split(' ')[-1]
+            unit_avail = datetime.strptime(str(year) + unit_avail_soup,'%Y%b. %d').strftime('%Y%m%d')
+        elif unit_avail_soup.split(' ')[-1] == "Now":
+            unit_avail = datetime.today().strftime('%Y%m%d')
         else:
-            unit_avail = unit_avail_soup.split(' ')[-1]
+            None
     else:
-        unit_avail = ""
+        unit_avail = None
     return unit_avail
--- src/crawler/get_property_url.py
+++ src/crawler/get_property_url.py
@@ -3,8 +3,10 @@ import configparser
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+from datetime import datetime
 import multiprocessing as mp
 
+# self defined functions
 import extraction_functions
 
 def get_property_urls(url, output_path):
@@ -33,15 +35,12 @@ def get_property_urls(url, output_path):
         f.write(str(all_links).encode('utf-8'))
     print("{} property urls are scraped.".format(len(all_links)))
 
-def get_property_html(urls_file, html_store_path):
+def get_property_html(url, html_store_path):
     headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7","Accept-Language": "zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7","Accept-Encoding": "gzip, deflate, br, zstd"}
-    with open(urls_file, 'rb') as f:
-        urls = eval(f.read().decode('utf-8'))
-        for url in urls:
-            response = requests.get(url, headers=headers)
-            soup = BeautifulSoup(response.text, 'html.parser')
-            with open(os.path.join(html_store_path, url.split('/')[-3]+'.html'), 'wb') as f:
-                f.write(soup.prettify().encode('utf-8'))
+    response = requests.get(url, headers=headers)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    with open(os.path.join(html_store_path, url.split('/')[-3]+'.html'), 'wb') as f:
+        f.write(soup.prettify().encode('utf-8'))
 
 def test_property_html(urls_file, html_store_path):
     for _, _, files in os.walk(html_store_path):
@@ -54,74 +53,59 @@ def test_property_html(urls_file, html_store_path):
             else:
                 print("All urls are scraped.")
 
-def extract_property_info(html_store_path, info_store_path):
-    for _, _, files in os.walk(html_store_path):
-        for file in files:
-            df = pd.DataFrame(columns=['name', 
-                                       'tel', 
-                                       'address',
-                                       'city',
-                                       'state',
-                                       'zip',
-                                       'neighborhood',
-                                       'built',
-                                       'units', 
-                                       'stories',
-                                       'management',
-                                       'unit_no',
-                                       'unit_beds',
-                                       'unit_baths',
-                                       'unit_price', 
-                                       'unit_sqft', 
-                                       'unit_avail'])
-            soup = BeautifulSoup(open(os.path.join(html_store_path, file), 'rb'), 'html.parser')
-            
-            # extract property info
-            name = extraction_functions.name(soup, "h1.propertyName")
-            tel = extraction_functions.tel(soup, "div.phoneNumber span")
-            address = extraction_functions.address(soup, "div.propertyAddressContainer span.delivery-address span:nth-child(1)")
-            city = extraction_functions.city(soup, "div.propertyAddressContainer span:nth-child(2)")
-            state = extraction_functions.state(soup, "div.propertyAddressContainer span.stateZipContainer span:nth-child(1)")
-            zip = extraction_functions.zip(soup, "div.propertyAddressContainer span.stateZipContainer span:nth-child(2)")
-            neighborhood = extraction_functions.neighborhood(soup, "div.propertyAddressContainer span.neighborhoodAddress > a")
-            built, units, stories = extraction_functions.built_units_stories(soup, "section.feesSection.feesSectionV2.js-viewAnalyticsSection div#profileV2FeesWrapper > div:nth-child(3) > div:nth-child(2)")
-            management = extraction_functions.management(soup, "img.logo")
+def extract_property_info(html_store_path, file, info_store_path, cols):
+    df = pd.DataFrame(columns=cols)
+    soup = BeautifulSoup(open(os.path.join(html_store_path, file), 'rb'), 'html.parser')
+    mtime = os.stat(os.path.join(html_store_path, file)).st_mtime
+    dttm = datetime.fromtimestamp(mtime).strftime('%Y-%m-%d %H:%M:%S')
 
-            # extract units info
-            plan_info = soup.select("div.pricingGridItem.multiFamily.hasUnitGrid")
-            for plan in plan_info:
-                unit_beds = extraction_functions.beds(plan, "span.detailsTextWrapper > span:nth-child(1)")
-                unit_baths = extraction_functions.baths(plan, "span.detailsTextWrapper > span:nth-child(2)")
-                unit_info = plan.select("li.unitContainer.js-unitContainer")
-                for unit in unit_info:
-                    unit_no = extraction_functions.unit_no(unit, "div.unitColumn.column span:nth-child(2)")
-                    unit_price = extraction_functions.unit_price(unit, "div.pricingColumn.column span:nth-child(2)")
-                    unit_sqft = extraction_functions.unit_sqft(unit, "div.sqftColumn.column span:nth-child(2)")
-                    unit_avail = extraction_functions.unit_avail(unit, "div.availableColumn.column span:nth-child(1)")
-                    curr_unit = pd.DataFrame([[name, 
-                                            tel, 
-                                            address,
-                                            city,
-                                            state,
-                                            zip,
-                                            neighborhood,
-                                            built,
-                                            units,
-                                            stories,
-                                            management,
-                                            unit_no, 
-                                            unit_beds, 
-                                            unit_baths, 
-                                            unit_price, 
-                                            unit_sqft, 
-                                            unit_avail]], columns=df.columns)
-                    df = pd.concat([df, curr_unit], ignore_index=True)
+    # extract property info
+    name = extraction_functions.name(soup, "h1.propertyName")
+    tel = extraction_functions.tel(soup, "div.phoneNumber span")
+    address = extraction_functions.address(soup, "div.propertyAddressContainer span.delivery-address > span:nth-child(1)")
+    city = extraction_functions.city(soup, "div.propertyAddressContainer span:nth-child(2)")
+    state = extraction_functions.state(soup, "div.propertyAddressContainer span.stateZipContainer > span:nth-child(1)")
+    zip = extraction_functions.zip(soup, "div.propertyAddressContainer span.stateZipContainer > span:nth-child(2)")
+    neighborhood = extraction_functions.neighborhood(soup, "div.propertyAddressContainer span.neighborhoodAddress > a.neighborhood")
+    built, units, stories = extraction_functions.built_units_stories(soup, "section.feesSection.feesSectionV2.js-viewAnalyticsSection div#profileV2FeesWrapper > div:nth-child(3) > div:nth-child(2)")
+    management = extraction_functions.management(soup, "img.logo")
 
-            # save to json and csv
-            if not df.empty:
-                df = df.drop_duplicates()
-                df.to_json(os.path.join(info_store_path, "json", file.split('.')[-2]+".json"), orient='records', lines=True)
-                df.to_csv(os.path.join(info_store_path, "csv", file.split('.')[-2]+".csv"), index=False)
+    # extract units info
+    plan_info = soup.select("div.pricingGridItem.multiFamily.hasUnitGrid")
+    for plan in plan_info:
+        unit_beds = extraction_functions.beds(plan, "span.detailsTextWrapper > span:nth-child(1)")
+        unit_baths = extraction_functions.baths(plan, "span.detailsTextWrapper > span:nth-child(2)")
+        unit_info = plan.select("li.unitContainer.js-unitContainer")
+        for unit in unit_info:
+            unit_no = extraction_functions.unit_no(unit, "div.unitColumn.column span:nth-child(2)")
+            unit_price = extraction_functions.unit_price(unit, "div.pricingColumn.column span:nth-child(2)")
+            unit_sqft = extraction_functions.unit_sqft(unit, "div.sqftColumn.column span:nth-child(2)")
+            unit_avail = extraction_functions.unit_avail(unit, "div.availableColumn.column span:nth-child(1)")
+            curr_unit = pd.DataFrame([[name, 
+                                    tel, 
+                                    address,
+                                    city,
+                                    state,
+                                    zip,
+                                    neighborhood,
+                                    built,
+                                    units,
+                                    stories,
+                                    management,
+                                    unit_no, 
+                                    unit_beds, 
+                                    unit_baths, 
+                                    unit_price, 
+                                    unit_sqft, 
+                                    unit_avail,
+                                    dttm]], columns=cols)
+            df = pd.concat([df, curr_unit], ignore_index=True)
+
+    # save to json and csv
+    if not df.empty:
+        df = df.drop_duplicates()
+        df.to_json(os.path.join(info_store_path, "json", file.split('.')[-2]+".json"), orient='records', lines=True)
+        df.to_csv(os.path.join(info_store_path, "csv", file.split('.')[-2]+".csv"), index=False)
 
 def compile_and_export_results(info_store_path, result_file):
     pass
@@ -134,6 +118,8 @@ def main():
     config.read(config_path)
     search_URL = config['user_config']['search_URL']
     chromedriver = config['user_config']['chromedriver_dir']
+    POOL_SIZE = os.cpu_count()
+    columns = ['name', 'tel', 'address', 'city', 'state', 'zip', 'neighborhood', 'built', 'units', 'stories', 'management', 'unit_no', 'unit_beds', 'unit_baths', 'unit_price', 'unit_sqft', 'unit_avail', 'dttm']
     
     # Set up input and output paths
     property_urls_file = os.path.join(current_dir, '../../data/property_urls.json')
@@ -141,10 +127,26 @@ def main():
     property_info_path = os.path.join(current_dir, '../../data/property_info/')
     result_file = os.path.join(current_dir, '../../data/results.csv')
 
-    #get_property_urls(search_URL, property_urls_file)
-    #get_property_html(property_urls_file, property_html_path)
-    #test_property_html(property_urls_file, property_html_path)
-    extract_property_info(property_html_path, property_info_path)
+    # STEP1: Get property URLs
+    # get_property_urls(search_URL, property_urls_file)
+    # STEP2: Get property HTML
+    pool = mp.Pool(POOL_SIZE)
+    with open(property_urls_file, 'rb') as f:
+        urls = eval(f.read().decode('utf-8'))
+        for url in urls:
+            pass
+            # pool.apply_async(get_property_html, args=(url, property_html_path))
+    pool.close()
+    pool.join()
+    test_property_html(property_urls_file, property_html_path)
+    # STEP3: Extract property info
+    pool = mp.Pool(POOL_SIZE)
+    for _, _, files in os.walk(property_html_path):
+        for file in files:
+            pool.apply_async(extract_property_info, args=(property_html_path, file, property_info_path, columns))
+    pool.close()
+    pool.join()
+    # STEP4: Compile and export results
     compile_and_export_results(property_info_path, result_file)
 
 if __name__ == '__main__':
